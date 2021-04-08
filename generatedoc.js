c7c8d4e047dcbfe6cb940639ab9181794aea2e8a
telkenes <telk@protonmail.com>
2021-04-08 20:59:03 +0700

    Changed package name to srav2 from sra-v2

--- generatedoc.js
+++ generatedoc.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const endpoints = require('./endpoints.js')
-let docs = '# SRA v2 wrapper\n\n**Note:** Use `null` if you want to leave the query empty\n**Example:**\n \`\`\`js\nconst sra = require(\'srav2\')\nsra.chatbot(\'hello\', \'keyhere\'\)\n\`\`\`\n\n## Functions\n'
+let docs = '# SRA v2 wrapper\n# Installation\nTo install this package, simply do `npm i telkenes/srav2` and in your code import it as a normal node module called `srav2` or `let sra = require(\'srav2\')`\n\n**Note:** Use `null` if you want to leave the query empty\n**Example:**\n \`\`\`js\nconst sra = require(\'srav2\')\nsra.chatbot(\'hello\', \'keyhere\'\)\n\`\`\`\n\n## Functions\n'
 endpoints.forEach(endpoint => {
     docs += `
 ### ${endpoint.func}
